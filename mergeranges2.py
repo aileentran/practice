1368f6bf90e030fc17e4b55a237b3cf1724b8669
Aileen Tran <aileen.tran27@gmail.com>
2020-04-07 12:39:35 -0700

    Commented out print statements

--- mergeranges2.py
+++ mergeranges2.py
@@ -29,27 +29,27 @@ def merge_ranges(times):
 	"""Condense ranges."""
 
 	times.sort()
-	print(times)
+	# print(times)
 
 	merged = []
 	# set to very first meeting
 	curr_start = times[0][0]
 	curr_end = times[0][1]
-	print('curr_start', curr_start)
-	print('curr_end', curr_end)
+	# print('curr_start', curr_start)
+	# print('curr_end', curr_end)
 
 	for time in times:
 		start = time[0]
 		end = time[1]
-		print('start', start)
-		print('end', end)
+		# print('start', start)
+		# print('end', end)
 
 		if start == curr_start and end == curr_end:
 			continue
 		elif start in range(curr_start, curr_end + 1) and end > curr_end:
 			curr_end = end
-			print('curr_start', curr_start)
-			print('curr_end', curr_end)
+			# print('curr_start', curr_start)
+			# print('curr_end', curr_end)
 		else:
 			merged.append((curr_start, curr_end))
 			curr_start = start
