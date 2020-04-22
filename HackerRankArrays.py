faccb042e6f7a00bfd2d1f6c60359fabc9eed37c
Aileen Tran <aileen.tran27@gmail.com>
2020-04-22 16:13:00 -0700

    Completed hourglass summin'

--- HackerRankArrays.py
+++ HackerRankArrays.py
@@ -13,5 +13,42 @@
 # sum = hourglass sum
 
 # return sum
+"""
+test case
+
+input:
+arr = [[1, 1, 1, 0, 0, 0],
+[0, 1, 0, 0, 0, 0],
+[1, 1, 1, 0, 0, 0],
+[0, 0, 2, 4, 4, 0],
+[0, 0, 0, 2, 0, 0],
+[0, 0, 1, 2, 4, 0]]
+
+output: 19
+"""
+
 # Complete the hourglassSum function below.
 def hourglassSum(arr):
+
+	greatestSum = None
+
+	r = 0
+	while r <= 3:
+
+		c = 0
+		while c <= 3:
+
+			print(f'[r {r}] [c {c}]')
+
+			hourglassSum = arr[r][c] + arr[r][c + 1] + arr[r][c + 2] + arr[r + 1][c + 1] + arr[r + 2][c] + arr[r + 2][c + 1] + arr[r + 2][c + 2]
+			print(hourglassSum)
+
+			if greatestSum == None or hourglassSum > greatestSum:
+				greatestSum = hourglassSum
+
+			c += 1
+
+		r += 1
+
+	return greatestSum
+		
