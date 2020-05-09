0e00b5c6441cf587d63413a5e708af5fa542585e
Aileen Tran <aileen.tran27@gmail.com>
2020-05-09 13:23:38 -0700

    Completed linked list version of function

--- kthtolastnode.py
+++ kthtolastnode.py
@@ -50,15 +50,57 @@ c.next = d
 d.next = e
 
 
+# runtime = O(n) runspace O(n)
+# def kth_to_last_node(k, head):
+# 	"""Return node kth from the end!"""
+# 	if k < 1:
+# 		raise ValueError('Cannot find less than 1st to last node!')
+
+# 	node_list = []
+
+# 	while head:
+# 		node_list.append(head)
+# 		head = head.next
+
+# 	return node_list[-k]
+
+"""
+To use the actual linked list: 
+
+errors! k < 1 and k is > length of list!
+use a counter for length of list 
+
+then take length of list - k 
+while i <= k
+walk thru the list
+head.next 
+
+return the head
+
+runtime O(n) runspace O(1)
+"""
+
 def kth_to_last_node(k, head):
-	"""Return node kth from the end!"""
 	if k < 1:
-		raise ValueError('Cannot find less than 1st to last node!')
+		raise ValueError('Cannot find less than 1 from the last node!')
 
-	node_list = []
+	curr = head
 
-	while head:
-		node_list.append(head)
+	num_items = 0
+
+	while curr:
+		num_items += 1
+		curr = curr.next
+
+	if k > num_items:
+		raise ValueError('k is greater than num of items in list!')
+
+	travel = num_items - k
+
+	i = 0
+
+	while i < travel:
 		head = head.next
+		i += 1
 
-	return node_list[-k]
+	return head
