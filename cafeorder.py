cdccc38dc4c86d7fdb6c9af4702b3523a2af3deb
Aileen Tran <aileen.tran27@gmail.com>
2020-05-13 18:47:24 -0700

    Completed waterpipes and cafeorder checker

---
+++ cafeorder.py
@@ -0,0 +1,50 @@
+"""
+input: 3 lists - takout, dinein, served
+output: boolean - True if served is in first-come first serve order!
+
+ints values are arbitrary
+
+"""
+
+# out of order
+oo_takeout = [1, 3, 5]
+oo_dinein = [2, 4, 6]
+oo_served = [1, 2, 4, 6, 5, 3]
+
+# in order
+in_takeout = [17, 8, 24]
+in_dinein = [12, 19, 2]
+in_served = [17, 8, 12, 19, 24, 2]
+
+def is_first_come_first_served(take_out_orders, dine_in_orders, served_orders):
+
+    # Check if we're serving orders first-come, first-served
+
+    
+
+    for served in served_orders:
+        if len(take_out_orders) > 0:
+            first_takeout = take_out_orders[0]
+        else:
+            first_takeout = None
+
+        if len(dine_in_orders) > 0:
+            first_dinein = dine_in_orders[0]
+        else:
+            first_dinein = None
+
+        if (served == first_takeout):
+            take_out_orders.remove(first_takeout)
+        elif (served == first_dinein):
+            dine_in_orders.remove(first_dinein)
+        else:
+            return False
+
+    # orders still unfulfilled
+    if len(take_out_orders) > 0 or len(dine_in_orders) > 0:
+        return False
+
+    return True
+
+print(is_first_come_first_served(oo_takeout, oo_dinein, oo_served)) #False
+print(is_first_come_first_served(in_takeout, in_dinein, in_served)) #True
--- waterpipes.py
+++ waterpipes.py
@@ -7,9 +7,39 @@ water can only move down, left, or right!
 """
 # clear path
 good = [
- [1, 0, 0, 1],
+ [0, 0, 1, 1],
  [1, 0, 1, 1],
  [1, 0, 1, 1]
+]
+
+good2 = [
+ [0, 1, 0, 1],
+ [1, 0, 0, 1],
+ [0, 0, 0, 0]
+]
+
+good3 = [
+ [0, 0, 1, 0],
+ [1, 0, 0, 1],
+ [0, 0, 0, 0]
+]
+
+split = [
+ [1, 0, 1, 0],
+ [0, 0, 0, 0],
+ [0, 1, 0, 1],
+ [0, 1, 0, 1],
+ [0, 1, 0, 1],
+ [1, 1, 0, 1]
+]
+
+split2 = [
+ [1, 1, 0, 1, 1],
+ [0, 0, 0, 0, 0],
+ [0, 1, 0, 1, 0],
+ [0, 1, 1, 1, 0],
+ [0, 1, 1, 1, 0],
+ [1, 1, 1, 1, 0]
 ]
 
 # blocked at top
@@ -33,41 +63,103 @@ diagonal = [
  [1, 0, 0, 1]
 ]
 
+# thoughts
+# find the starting positions = 0s in first row
+# have visited
+# at the start
+# check directions
+# make sure directions are in bounds
+# have dict for direction tuple AND 0 or 1 (pipe/wall)
+
+# check to see if any of the things around are pipes
+# if run into pipe, break check
+# if no pipes, return false
+
+# then loop  through directions dictionary
+# if there is a pipe and direction is nOT in visted: set current to that direction
+# append it into visited
+
 def waterpipes(grid):
     """Check if there is a path for water to flow!"""
-    visited = []
-    
-    pos = []
+    nrows = len(grid)
+    ncols = len(grid[0])
 
-    for i,row in enumerate(grid):
+    # check for entire walls = fail early 
+    for row in grid:
         if 0 not in row:
+            return False 
+
+    start = []
+    visited = []
+
+    for i, col in enumerate(grid[0]):
+        if col == 0:
+            start.append((0, i))
+    # print(start)
+    
+    pos = start.pop()
+    row, col = pos
+    print('before', start)
+    while True:
+        # print('position', (row, col))
+        print('start', start)
+        # at the bottom of grid!
+        if row == len(grid) - 1:
+            break
+
+        visited.append((row, col))
+        # set up directions!
+        down = (row + 1, col)
+        left = (row, col - 1)
+        right = (row, col + 1)
+
+        directions = [down, left, right]
+        
+        pipe_or_wall = {}
+        # check if directions are in bounds
+        for direction in directions:
+            row, col = direction 
+            
+            if (row >= 0) and (row < nrows) and (col >= 0) and (col < ncols):
+                pipe_or_wall[direction] = grid[row][col]
+
+        any_pipes = False
+
+        for direction in pipe_or_wall:
+            if pipe_or_wall[direction] == 0:
+                any_pipes = True
+
+        # no where for the water to flow!
+        if not any_pipes and len(start) > 0:
+            row, col = start.pop()
+
+        elif not any_pipes:
             return False
 
-        print('row', row)
-        # print('row idx', i)
-        for k, col in enumerate(row):
-            visited.append((i, k))
-            print('col', col)
-            # print('col idx', k)
-            val = grid[i][k]
-            # print('val', val)
-            if val == 0:
-                pos.append((i, k))
+        # print(pipe_or_wall)
+        # print('visited', visited)
 
-    # down = [row + 1][col]
-    # left = [row][col - 1]
-    # right = [row][col + 1]
-    
-    # for p in pos:
-    #     row, col = p 
+        for direction in pipe_or_wall:
+            if pipe_or_wall[direction] == 0 and direction not in visited:
+                start.append(direction)
+                row, col = direction
+                break
 
 
-    print(pos)
-    # print(visited)
+
+   
+
+
+
+        
 
     return True
 
-print(waterpipes(good)) #True
+# print(waterpipes(good)) #True
 # print(waterpipes(top_blocked)) #False
 # print(waterpipes(bottom_blocked)) #False
 # print(waterpipes(diagonal)) #False
+print(waterpipes(good2)) #True
+print(waterpipes(good3)) #True
+print(waterpipes(split)) #True
+print(waterpipes(split2)) #True
