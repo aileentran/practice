c0efe39eced5559a9a4d454b0880f8fef3412707
Aileen Tran <aileen.tran27@gmail.com>
2020-06-17 15:30:58 -0700

    WIP edge case of multiplying 2 negative numbers

--- highestproduct.py
+++ highestproduct.py
@@ -4,11 +4,31 @@
 def highest_product_of_3(list_of_ints):
 
     # Calculate the highest product of three numbers
+    
+    # catching lists with less than 3 ints!
     if len(list_of_ints) < 3:
         raise ValueError('Whoopsidoodle! Need at least 3 ints!')
 
 
-    return 0
+    ints = []
+
+    i = 1
+
+    while i <= 3:
+
+        largest = max(list_of_ints)
+        idx = list_of_ints.index(largest)
+        largest = list_of_ints.pop(idx)
+        ints.append(largest)
+
+        i += 1
+
+    product = 1
+
+    for num in ints:
+        product *= num
+
+    return product
 
 print(highest_product_of_3([1, 2, 3, 4])) #24
 print(highest_product_of_3([6, 1, 3, 5, 7, 8, 2])) #336
