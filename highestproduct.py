5287b43b670e13c6a53d9ee8a7109714f59fcc34
Aileen Tran <aileen.tran27@gmail.com>
2020-06-17 16:18:59 -0700

    Setting up to account for edge case of 2 large negative numbers

--- highestproduct.py
+++ highestproduct.py
@@ -1,6 +1,40 @@
 # input: list of ints, always has at least 3 ints!
 # output: int - highest product from 3 ints!
 
+# def highest_product_of_3(list_of_ints):
+
+#     # Calculate the highest product of three numbers
+    
+#     # catching lists with less than 3 ints!
+#     if len(list_of_ints) < 3:
+#         raise ValueError('Whoopsidoodle! Need at least 3 ints!')
+
+#     list_of_ints.sort()
+#     print(list_of_ints)
+
+#     # check to see if there are 2 negative numbers
+#     if list_of_ints[0] < 0 and list_of_ints[1] < 0:
+#         return 'two negative numbers!'
+
+#     else:
+
+#         product = 1
+
+#         i = 1
+
+#         while i <= 3:
+
+#             largest = max(list_of_ints)
+#             idx = list_of_ints.index(largest)
+#             largest = list_of_ints.pop(idx)
+#             product *= largest
+
+#             i += 1
+
+#     return product
+
+
+# accounting for 2 negative numbers!
 def highest_product_of_3(list_of_ints):
 
     # Calculate the highest product of three numbers
@@ -9,28 +43,10 @@ def highest_product_of_3(list_of_ints):
     if len(list_of_ints) < 3:
         raise ValueError('Whoopsidoodle! Need at least 3 ints!')
 
+    list_of_ints.sort()
+    print(list_of_ints)
 
-    # ints = []
-    product = 1
-
-    i = 1
-
-    while i <= 3:
-
-        largest = max(list_of_ints)
-        idx = list_of_ints.index(largest)
-        largest = list_of_ints.pop(idx)
-        product *= largest
-        # ints.append(largest)
-
-        i += 1
-
-    # product = 1
-
-    # for num in ints:
-    #     product *= num
-
-    return product
+   
 
 print(highest_product_of_3([1, 2, 3, 4])) #24
 print(highest_product_of_3([6, 1, 3, 5, 7, 8, 2])) #336
