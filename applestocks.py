c0e9e072858cc400192a4574d203183d321475c8
Aileen Tran <aileen.tran27@gmail.com>
2020-06-16 17:02:58 -0700

    Set up function to get max profits

---
+++ applestocks.py
@@ -0,0 +1,9 @@
+def get_max_profit(stock_prices):
+
+    # Calculate the max profit
+
+
+print(get_max_profit([1, 5, 3, 2])) #4
+print(get_max_profit([7, 2, 8, 9])) #7
+print(get_max_profit([9, 7, 4, 1])) #-2
+print(get_max_profit([1, 1, 1, 1])) #0
