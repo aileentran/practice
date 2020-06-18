7c1ef253aab5b49f7f711e88b2f66f64e81a536c
Aileen Tran <aileen.tran27@gmail.com>
2020-06-17 17:09:39 -0700

    Completed function. WIP for O(n) time and space

--- productofallotherints.py
+++ productofallotherints.py
@@ -8,6 +8,33 @@ def get_products_of_all_ints_except_at_index(int_list):
     if len(int_list) < 2:
         raise ValueError('Need at least 2 numbers in list!')
 
+    result = []
+
+    i = 0
+
+    while i < len(int_list):
+        curr = int_list[i]
+        # print('curr', curr)
+
+        product = 1
+
+        k = 0
+
+        while k < len(int_list):
+            num = int_list[k]
+            # print('num', num)
+            
+            if k != i:
+                product *= num
+                # print('product', product)
+            k += 1
+
+        result.append(product)
+
+        i += 1
+
+    return result
+
 
 print(get_products_of_all_ints_except_at_index([1, 2, 3])) 
 #[6, 3, 2]
@@ -15,12 +42,12 @@ print(get_products_of_all_ints_except_at_index([8, 2, 4, 3, 1, 5]))
 #[120, 480, 240, 320, 960, 192]
 print(get_products_of_all_ints_except_at_index([6, 2, 0, 3])) 
 #[0, 0, 36, 0]
-print(get_products_of_all_ints_except_at_index([4, 0, 9, 1, 0])
+print(get_products_of_all_ints_except_at_index([4, 0, 9, 1, 0]))
 # [0, 0, 0, 0, 0]
 print(get_products_of_all_ints_except_at_index([-3, 8, 4])) 
 #[32, -12, -24]
 print(get_products_of_all_ints_except_at_index([-7, -1, -4, -2]))
-#[-8, -56, -14, -28]
+[-8, -56, -14, -28]
 # account for inputs w/1 or less ints
 # print(get_products_of_all_ints_except_at_index([]))
 # print(get_products_of_all_ints_except_at_index([1]))
