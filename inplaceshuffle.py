c0ca7110dd328538416c4e793f04ec5e51ecac61
Aileen Tran <aileen.tran27@gmail.com>
2020-06-18 16:13:34 -0700

    WIP checking inplaceshuffle algorithm

--- inplaceshuffle.py
+++ inplaceshuffle.py
@@ -8,16 +8,27 @@ def get_random(floor, ceiling):
 def shuffle(the_list):
 
     # Shuffle the input in place
+    i = 0
+
+    while i < len(the_list):
+
+    	ran = get_random(1, len(the_list))
+    	ran_idx = the_list.index(ran)
+    	new = the_list.pop(ran_idx)
+    	the_list.insert(0, new)
+
+    	i += 1
+
+    return the_list
+
+    
 
 
-    pass
 
 
-# get_random(1, 5)
+sample_list = [1, 2, 3, 4, 5]
+print('Sample list:', sample_list)
 
-# sample_list = [1, 2, 3, 4, 5]
-# print('Sample list:', sample_list)
-
-# print('Shuffling sample list...')
-# shuffle(sample_list)
-# print(sample_list)
+print('Shuffling sample list...')
+shuffle(sample_list)
+print(sample_list)
