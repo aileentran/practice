53096708fac4aab1676c863bb7d4cdc4f53406d9
Aileen Tran <aileen.tran27@gmail.com>
2020-06-18 16:04:40 -0700

    Initial commit

---
+++ inplaceshuffle.py
@@ -0,0 +1,23 @@
+import random
+
+
+def get_random(floor, ceiling):
+    return random.randrange(floor, ceiling + 1)
+
+
+def shuffle(the_list):
+
+    # Shuffle the input in place
+
+
+    pass
+
+
+# get_random(1, 5)
+
+# sample_list = [1, 2, 3, 4, 5]
+# print('Sample list:', sample_list)
+
+# print('Shuffling sample list...')
+# shuffle(sample_list)
+# print(sample_list)
