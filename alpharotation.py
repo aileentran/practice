e285e7686b45cf413aa3fb92643330bddefc1e0a
Aileen Tran <aileen.tran27@gmail.com>
2020-06-22 15:59:24 -0700

    Initial commit - setting up problem and test cases

---
+++ alpharotation.py
@@ -0,0 +1,12 @@
+# input: list of words in alpha order! 
+# output: idx of rotation point 
+
+def find_rotation_point(words):
+
+    # Find the rotation point in the list
+
+
+print(find_rotation_point(['cape', 'cake'])) #1
+print(find_rotation_point(['grape', 'orange', 'plum','radish', 'apple'])) #4
+print(find_rotation_point(['ptolemaic', 'retrograde', 'supplant',
+                                      'undulate', 'xenoepist', 'asymptote','babka', 'banoffee', 'engender','karpatka', 'othellolagkage'])) #5
