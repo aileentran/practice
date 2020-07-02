de14580c6d55577c7f865fd7618c32a39eb7d004
Aileen Tran <aileen.tran27@gmail.com>
2020-07-02 14:28:00 -0700

    Completed convertingtimes and WIP birthdaycandles

---
+++ birthdaycandles.py
@@ -0,0 +1,39 @@
+# input: list of ints
+# output: num of candles blown out
+
+# ints = candle number which is correlated to its height
+# larger num = taller candle
+# neice will blow out tallest candles 
+
+# thoughts
+
+# empty result variable
+# tallest variable
+
+# loop through the list
+# if num > tallest
+# result = 0
+# tallest = num AND result += 1
+# if num == tallest
+# result increases by one
+# if it's smaller, we keep going
+
+# outside loop
+# return result!
+
+# Complete the birthdayCakeCandles function below.
+def birthdayCakeCandles(ar):
+	blown = 0
+	tallest = 0
+
+	for candle in ar:
+		if candle > tallest:
+			blown = 0
+			tallest = candle
+			blown += 1
+		elif candle == tallest:
+			blown += 1
+
+	return blown
+
+print(birthdayCakeCandles([3, 2, 1, 3])) #2 bc blow out both 3s!
---
+++ convertingtime.py
@@ -0,0 +1,34 @@
+# input: string hh:mm:ssAM or hh:mm:ssPM
+# output: hh:mm:ss in military time! 
+
+# always valid input
+
+# thoughts
+# isolate AM or PM into variable
+# if AM -> return hh:mm:ss as is 
+# if PM -> convert hh into an int + 12 back into int and return hh:mm:ss
+
+
+def timeConversion(s):
+    meridian = s[-2:]
+    hour = s[:2]
+    
+    if meridian == 'AM' and hour == '12':
+        return f'00{s[2:-2]}'
+    
+    elif meridian == 'AM' or (meridian == 'PM' and hour == '12'):
+        return s[:-2]
+    
+    else:    
+        convert = int(hour) + 12
+        convert = str(convert)
+
+        return f'{convert}{s[2:-2]}'
+
+    	
+
+
+print(timeConversion('07:05:45PM')) #19:05:45
+print(timeConversion('07:05:45AM')) #07:05:45
+print(timeConversion('12:40:22AM')) #00:40:22
+print(timeConversion('12:45:54PM')) #12:45:54
