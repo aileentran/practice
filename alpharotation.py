3524f73053c44544f72a23dbd0f0738915cd6c11
Aileen Tran <aileen.tran27@gmail.com>
2020-07-02 14:21:03 -0700

    WIP alpharotation. Completed convertingtime

--- alpharotation.py
+++ alpharotation.py
@@ -4,9 +4,28 @@
 def find_rotation_point(words):
 
     # Find the rotation point in the list
+    alpha = 'abcdefghijklmnopqrstuvwxyz'
+
+    floor = -1
+    ceil = len(words)
+
+    idx = 0
+
+    for word in words:
+        print(word)
+        for char in word:
+            print('char', char)
+            i = 0
+
+            while i < len(alpha):
+                letter = alpha[i]
+                print('letter', letter)
+                # if char == letter:
+
+                i += 1
 
 
 print(find_rotation_point(['cape', 'cake'])) #1
-print(find_rotation_point(['grape', 'orange', 'plum','radish', 'apple'])) #4
-print(find_rotation_point(['ptolemaic', 'retrograde', 'supplant',
-                                      'undulate', 'xenoepist', 'asymptote','babka', 'banoffee', 'engender','karpatka', 'othellolagkage'])) #5
+# print(find_rotation_point(['grape', 'orange', 'plum','radish', 'apple'])) #4
+# print(find_rotation_point(['ptolemaic', 'retrograde', 'supplant',
+#                                       'undulate', 'xenoepist', 'asymptote','babka', 'banoffee', 'engender','karpatka', 'othellolagkage'])) #5
