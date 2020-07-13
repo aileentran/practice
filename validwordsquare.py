9fe37c172d1c0e0faad054ac0feaffd56d2ba4e6
Aileen Tran <aileen.tran27@gmail.com>
2020-07-13 14:24:59 -0700

    WIP accessing columns

--- validwordsquare.py
+++ validwordsquare.py
@@ -3,23 +3,54 @@
 
 # Q's: would ever get anything besides strings? chars only?
 
+# thoughts
+# row = words[i]
+# col1 = words[0][0], words[1][0], words[2][0], words[3][0]
+# col2 = words[0][1], words[1][1], words[2][1], words[3][1]
+# col3 = words[0][2], words[1][2], words[2][2], words[3][2]
+# col4 = words[0][3], words[1][3], words[2][3], words[3][3]
+
+# make an array of cols?
+
+# while loop through both words and cols
+# check if each element matches
+# if not, false
+
+# outside loop, return true!
+
 def validWordSquare(words):
-	"""Check if valid word square where row[i] == col[i]."""
+    """Check if valid word square where row[i] == col[i]."""
+    cols = []
+    w = ''
 
+    y = 0
 
-validWordSquare([
-  "abcd",
-  "bnrt",
-  "crmy",
-  "dtye"
-]) #true
+    while y < len(words[0]):
+        x = 0
 
-validWordSquare([
-  "abcd",
-  "bnrt",
-  "crm",
-  "dt"
-]) #true
+        while x < len(words):
+            print(words[x][y])
+            x += 1
+        
+
+        y += 1
+
+# runtime ??
+# runspace ??
+
+# validWordSquare([
+#   "abcd",
+#   "bnrt",
+#   "crmy",
+#   "dtye"
+# ]) #true
+
+# validWordSquare([
+#   "abcd",
+#   "bnrt",
+#   "crm",
+#   "dt"
+# ]) #true
 
 validWordSquare([
   "ball",
