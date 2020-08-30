baa92d1732c6bfc7bc8a57221047089f5d98e95a
Aileen Tran <aileen.tran27@gmail.com>
2020-08-29 19:01:52 -0700

    WIP linked list implementation

--- queue.py
+++ queue.py
@@ -1,6 +1,7 @@
 # Functions - enqueue, dequeue, peek
 
 # Implement with list
+# Assumptions: Start of Queue @ idx 0; End of list at the back
 class Queue(object):
     def __init__(self):
         self.items = []
@@ -15,20 +16,52 @@ class Queue(object):
         return self.items[0]
 
 
-
 # Implement with linked list
+# Assumptions: Start of Queue at head node; End of queue at tail
+
+class Node(object):
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class Queue_ll(object):
+
+    def __init__(self):
+        self.front = None
+        self.back = None
+
+    def enqueue(self, item):
+        if self.front == None:
+            self.front = Node(item)
+            self.back = Node(item)
+        else:
+            self.back.next = Node(item)
+            self.back = Node(item)
 
 
-# Cases
-line = Queue()
-print(line.items)
+# Test1 - List implementation
+# line = Queue()
+# print(line.items)
+
+# line.enqueue('a')
+# line.enqueue('b')
+# line.enqueue('c')
+# line.enqueue('d')
+# print(line.items)
+
+# print(line.dequeue())
+
+# print(line.peek())
+
+# Test2 - Linked List implementation
+line = Queue_ll()
+# print(line)
+# print(line.front)
+# print(line.back)
 
 line.enqueue('a')
 line.enqueue('b')
-line.enqueue('c')
-line.enqueue('d')
-print(line.items)
-
-print(line.dequeue())
-
-print(line.peek())
+# line.enqueue('c')
+print(line.front.data)
+print(line.front.next.data)
+# print(line.back.data)
