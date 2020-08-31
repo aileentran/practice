8a6a765313598fcd6d0c365a205e67e46f16fea7
Aileen Tran <aileen.tran27@gmail.com>
2020-08-30 18:02:39 -0700

    Edited where to insert new nodes

--- binarytree.py
+++ binarytree.py
@@ -15,37 +15,44 @@ class BinaryTree(object):
 		return self.root.value
 
 	def insert(self, value):
-
 		current_node = self.root
-		# print(current_node.value)
 
 		while True:
-			if value > current_node.value and current_node.left == None:
-				current_node.left = Node(value)
-				break
-			
-			if value < current_node.value and current_node.right == None:
+			if value > current_node.value and current_node.right == None:
 				current_node.right = Node(value)
 				break
 			
-			if value > current_node.value and current_node.left != None:
-				current_node = current_node.left
-			elif value < current_node.value and current_node.right != None:
+			if value < current_node.value and current_node.left == None:
+				current_node.left = Node(value)
+				break
+			
+			if value > current_node.value and current_node.right != None:
 				current_node = current_node.right
+			elif value < current_node.value and current_node.left != None:
+				current_node = current_node.left
+
+	def print_in_order(self):
+		current_node = self.root
+
+		while current_node.left.value:
+			print(current_node.value)
+
+			current_node = current_node.left
+			print(current_node.value)
 
 tiny_tree = BinaryTree(5)
 # print(tiny_tree)
 # print(tiny_tree.root)
 
-# print(tiny_tree.print_root())
+print(tiny_tree.print_root())
 
 tiny_tree.insert(7)
 root = tiny_tree.root
-print(root.left.value)
-
-tiny_tree.insert(3)
 print(root.right.value)
 
+tiny_tree.insert(3)
+print(root.left.value)
+
 tiny_tree.insert(1)
-print(root.right.right.value)
-print(root.left.left)
+print(root.left.left.value)
+print(root.right.right)
