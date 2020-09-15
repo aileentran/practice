2ed75e4e70dbac51cdd92d5ab33b56725e6f3931
Aileen Tran <aileen.tran27@gmail.com>
2020-09-15 13:28:28 -0700

    Initial commit

---
+++ 2darray.py
@@ -0,0 +1,25 @@
+"""
+Hackerrank Interview Prep - Arrays
+
+Given a 6 x 6 array, there are 16 hour glasses
+
+Calculate sum of every hour glass and return max sum. 
+"""
+
+# thoughts
+# create empty max_sum
+
+# establish the configuration of the hour glass -> helper function
+# traverse through the 2d array 
+# have an empty sum variable
+# get the configuration and add each element to the sum
+# if the sum is > max_sum or max_sum is none, set max_sum to sum
+
+# return the max_sum 
+
+def max_sum(grid):
+	maximum_sum = None
+
+	
+
+def hourglass(grid):
--- largesttimeforgivendigits.py
+++ largesttimeforgivendigits.py
@@ -8,10 +8,25 @@ The smallest 24 hour time is 00:00, and the largest is 23:59.  Starting from 00:
 Return the answer as a string of length 5.  If no valid time can be made, return an empty string.
 
 """
+# thoughts
+# input: list of nums
+# output: string -> "hh:mm"
+# h1 <= 2 max nums in this range is 4; min 1
+# h2 <= 4 max is 3; min 1
+# m1 <= 5 max is 2
+# m2 <= 9 max is 1
 
 def largest_time_from_digits(nums):
+	# counters for hh:mm
+	h1 = h2 = m1 = m2 = 0
 
+	for num in nums:
+		if num <= 2:
+			h1 += 1
 
+		if 
+
+	
 """
 Example 1:
 Input: [1,2,3,4]
@@ -22,7 +37,7 @@ Input: [5,5,5,5]
 Output: ""
 
 """
-# nums = [1,2,3,4]
+nums = [1,2,3,4]
 # nums = [5,5,5,5]
 
 print(largest_time_from_digits(nums))
