37164702f8fc515b3383d6f4ce213f026db9300c
Aileen Tran <aileen.tran27@gmail.com>
2020-09-17 12:06:09 -0700

    Passed test cases

--- arraymanipulation.py
+++ arraymanipulation.py
@@ -22,3 +22,35 @@ Hackerrank Interview Prep - Arrays
 # see if need to loop through list to find largest sum
 # return largest sum
 
+def arrayManipulation(n, queries):
+    list_of_n = [0] * n
+    max_sum = 0
+
+    for q in queries:
+        start, end, summand = q
+        print('start', start)
+        print('end', end)
+        print('summand', summand)
+
+        i = start - 1
+        while i < end:
+            list_of_n[i] += summand
+            if list_of_n[i] > max_sum:
+                max_sum = list_of_n[i]
+            i += 1
+        print(list_of_n)
+    
+    return max_sum
+
+# Tests
+n1 = 5
+queries1 = [[1, 2, 100], [2, 5, 100], [3, 4, 100]]
+# output: 200
+
+n2 = 10
+queries2 = [[1, 5, 3], [4, 8, 7], [6, 9, 1]]
+# output: 10
+
+n3 = 10
+queries3 = [[2, 6, 8], [3, 5, 7], [1, 8, 1], [5, 9, 15]]
+# output: 31
