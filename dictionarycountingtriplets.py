7aa51809e87d693ec25363c1d616b5594de658cb
Aileen Tran <aileen.tran27@gmail.com>
2020-09-19 12:16:31 -0700

    Initial commit

---
+++ dictionarycountingtriplets.py
@@ -0,0 +1,11 @@
+"""
+Hackerrank Interview Prep - Dictionaries/Hash Maps
+"""
+
+# Notes and thoughts
+# input: list of integers, int - common ratio
+# output: number of triplets
+# Complete the countTriplets function below.
+def countTriplets(arr, r):
+
+	
