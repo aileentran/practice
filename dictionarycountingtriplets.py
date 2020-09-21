983175eef99ce5409b5f956235c4c63b782d0ab2
Aileen Tran <aileen.tran27@gmail.com>
2020-09-21 13:45:35 -0700

    Included expected outcome

--- dictionarycountingtriplets.py
+++ dictionarycountingtriplets.py
@@ -7,7 +7,7 @@ Hackerrank Interview Prep - Dictionaries/Hash Maps
 def countTriplets2(arr, r):
     nums = {}
     triplets = 0
-
+    
     for num in arr:
         if num not in nums:
             nums[num] = 1
@@ -33,8 +33,8 @@ def countTriplets2(arr, r):
 nums = []
 
 for i in range(1, 101):
-	nums.append(1)
-print(nums)
+    nums.append(1)
+print(countTriplets2(nums, 1)) #161700, getting 1000000
 ################################################################
 # Notes and thoughts
 # input: list of integers, int - common ratio
@@ -43,28 +43,28 @@ print(nums)
 # first iteration with only one num has multiple
 # ex [1, 3, 9, 9, 27]
 # Complete the countTriplets function below.
-def countTriplets1(arr, r):
-	nums = {}
-    triplets = 0
+# def countTriplets1(arr, r):
+#     nums = {}
+#     triplets = 0
 
-    for num in arr:
-        if num not in nums:
-            nums[num] = 1
-        else:
-            nums[num] += 1
-    print(nums)
-    for num in nums:
-        sum_nums = 0
-        second = num * r
-        third = second * r
-        print('potential triplet', num, second, third)
-        if second in nums and third in nums:
-            print('triplet', num, second, third)
-            # sum_nums += nums[num]
-            # sum_nums += nums[second]
-            # sum_nums += nums[third]
-            sum_nums = max(nums[num], nums[second], nums[third])
-            triplets += sum_nums
-            print('sum_nums', sum_nums)
-            print('triplets', triplets)
-    return triplets
+#     for num in arr:
+#         if num not in nums:
+#             nums[num] = 1
+#         else:
+#             nums[num] += 1
+#     print(nums)
+#     for num in nums:
+#         sum_nums = 0
+#         second = num * r
+#         third = second * r
+#         print('potential triplet', num, second, third)
+#         if second in nums and third in nums:
+#             print('triplet', num, second, third)
+#             # sum_nums += nums[num]
+#             # sum_nums += nums[second]
+#             # sum_nums += nums[third]
+#             sum_nums = max(nums[num], nums[second], nums[third])
+#             triplets += sum_nums
+#             print('sum_nums', sum_nums)
+#             print('triplets', triplets)
+#     return triplets
