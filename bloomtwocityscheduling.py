97a6ebca903ffdeee28dc94796928fc500b80f0b
Aileen Tran <aileen.tran27@gmail.com>
2020-09-23 12:57:12 -0700

    WIP

--- bloomtwocityscheduling.py
+++ bloomtwocityscheduling.py
@@ -6,7 +6,7 @@ Leetcode 1029. Two City Scheduling
 # input: 2d array of costs per person [fly to city a, fly to city b]
 # output: num - sum of MIN cost to fly EVERYONE out
 
-def twoCitySchedCost(self, costs: List[List[int]]) -> int:
+def twoCitySchedCost(costs):
         min_cost = 0
         
         for cost in costs:
@@ -14,5 +14,12 @@ def twoCitySchedCost(self, costs: List[List[int]]) -> int:
             print('cityA', cityA)
             print('cityB', cityB)
             min_cost += min(cityA, cityB)
+            print('cheaper city', min(cityA, cityB))
+            print(min_cost)
             
         return min_cost
+
+costs1 = [[259,770],[448,54],[926,667],[184,139],[840,118],[577,469]]
+# expect - 1859; initial sol'n output: 1706
+
+print(twoCitySchedCost(costs1))
