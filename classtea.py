1ea25022764851116fe00d616f5afe2f7e18e482
Aileen Tran <aileen.tran27@gmail.com>
2020-09-27 22:25:06 -0700

    Adjusted class attribute

--- classtea.py
+++ classtea.py
@@ -1,7 +1,8 @@
 class Tea(object):
-	def __init__(self, name, region):
+	def __init__(self, name, tea_type, region):
 		self.name = name
 		self.region = region
+		self.tea_type = tea_type
 		self.temperature = 0
 
 	def get_name(self):
@@ -10,29 +11,34 @@ class Tea(object):
 	def get_region(self):
 		return self.region
 
-	def tea_temperature(self, tea_type):
-		if self.region == 'Japan' and self.name == 'gyokuro' and tea_type == 'green':
+	def tea_temperature(self):
+		if self.region == 'Japan' and self.tea_type == 'gyokuro':
 			self.temperature = 122
 
-		elif self.region == 'Japan' and self.name == 'sencha' and tea_type == 'green':
+		elif self.region == 'Japan' and self.tea_type == 'sencha':
 			self.temperature = 160
 
-		elif tea_type == 'green' and self.region != 'Japan':
+		elif self.tea_type == 'green' and self.region != 'Japan':
 			self.temperature = 175
 
-		elif tea_type == 'oolong' and self.region != 'Japan':
+		elif self.tea_type == 'oolong' and self.region != 'Japan':
 			self.temperature = 195
 
-		elif tea_type == 'black' and tea_type == 'puerh' and self.region != 'Japan':
+		elif self.tea_type == 'black' and tea_type == 'puerh' and self.region != 'Japan':
 			self.temperature = 212
 
 		return self.temperature
 
-
-
 	def __str__(self):
 		return str(self.name) + ' ' + str(self.region)
 
-gyokuro = Tea('gyokuro', 'Japan')
+# def teaware(teas):
+
+
+gyokuro = Tea('yame gyokuro', 'gyokuro', 'Japan')
+fukamushi = Tea('takumi fukamushi', 'sencha', 'Japan')
+baozhong = Tea('baozhong', 'oolong', 'China')
 # print(gyokuro.get_region())
-print(gyokuro.tea_temperature('green'))
+print(gyokuro.tea_temperature())
+print(fukamushi.tea_temperature())
+print(baozhong.tea_temperature())
