eefc9ac772e32a2142a872628edebe423aec4d02
Aileen Tran <aileen.tran27@gmail.com>
2020-10-06 13:57:58 -0700

    Another run through with run time and run space

--- twocityscheduling.py
+++ twocityscheduling.py
@@ -5,6 +5,88 @@ A company is planning to interview 2n people. Given the array costs where costs[
 
 Return the minimum cost to fly every person to a city such that exactly n people arrive in each city.
 """
+# Notes:
+# input: array of costs - idx = a person; [cityA flight, cityB flight]
+# output: number - minimum cost to fly every person to a city
+# *half the people in city A; half in city B
+
+# minimum cost variable to 0 
+# list [cost difference, person's idx] --> sort that in descending order
+
+# counter for city A, counter for city B half thepeople per city
+# loop through cost difference list
+# prioritize them n the cheaper city until a city is full
+# push all the other people in the empty city
+# while doing this, adding the flight cost to the minimum cost vairable
+
+# outside the loop
+# return the minimum cost
+# talk time: 16 min
+
+def twoCitySchedCost(costs):
+    min_cost = 0 
+    cost_diff = []
+
+    for person, cost in enumerate(costs):
+        flightA, flightB = cost
+        diff = abs(flightA - flightB)
+        cost_diff.append([diff, person])
+
+    cost_diff.sort(reverse = True)
+    
+    cityA = len(costs) // 2
+    cityB = len(costs) // 2
+
+    for diff in cost_diff:
+        price_diff, idx = diff 
+        costA, costB = costs[idx]
+        if (costA < costB and cityA > 0) or cityB == 0:
+            cityA -= 1
+            min_cost += costA
+        else:
+            cityB -= 1
+            min_cost += costB
+
+    return min_cost
+
+# run time: O(n)
+# run space: cost_diff -> n space; everything else is a constant; O(n)
+
+
+costs1 = [[10,20],[30,200],[400,50],[30,20]] #expected: 110
+costs2 = [[259,770],[448,54],[926,667],[184,139],[840,118],[577,469]] # expected: 1859
+costs3 = [[515,563],[451,713],[537,709],[343,819],[855,779],[457,60],[650,359],[631,42]] # expected: 3086
+
+
+print(twoCitySchedCost(costs3))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 # Notes and thoughts
 # input: array of arrays - array[person] = [cityAflight, cityBflight]
 # output: num - minimum cost
@@ -24,7 +106,7 @@ Return the minimum cost to fly every person to a city such that exactly n people
 # pop them in there 
 # once a city fills up, just stick the rest in the other city :o 
 
-def twoCitySchedCost(costs):
+def twoCitySchedCost1(costs):
     min_cost = 0
     difference = [] #[cost difference, idx of person]
 
@@ -60,11 +142,3 @@ def twoCitySchedCost(costs):
         # print('\n')
 
     return min_cost
-
-
-costs1 = [[10,20],[30,200],[400,50],[30,20]] #expected: 110
-costs2 = [[259,770],[448,54],[926,667],[184,139],[840,118],[577,469]] # expected: 1859
-costs3 = [[515,563],[451,713],[537,709],[343,819],[855,779],[457,60],[650,359],[631,42]] # expected: 3086
-
-
-print(twoCitySchedCost(costs1))
