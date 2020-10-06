ab2652c40088feced67cdb9c5fb4025055a1306b
Aileen Tran <aileen.tran27@gmail.com>
2020-10-05 17:38:12 -0700

    Initial thoughts

--- minstepstonum.py
+++ minstepstonum.py
@@ -6,6 +6,12 @@ multiply by 2
 integer division by 3 (e.g. 10 / 3 = 3) --> // 3 for python
 Find the minimum number of steps required to generate n from 1.
 """
+# Notes and thoughts
+# input: number - what w'ere trying to get to
+# output: number - min number of steps to generate input num
+# have a seen list
+# start w/ 1, *= 2 until past goal number AND if //= 3 already in seen
+# // 3 until we get to number 
 def min_steps(num):
 
 # Tests
