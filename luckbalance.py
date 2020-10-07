b3ec89adb24d78ab990670e3b5434f91ea9f3e97
Aileen Tran <aileen.tran27@gmail.com>
2020-10-07 13:31:19 -0700

    Initial solution

--- luckbalance.py
+++ luckbalance.py
@@ -10,11 +10,73 @@ Hackerrank Interview Prep - Greedy Alg - Luck Balance
 # L: lose - balance += luck; win - balance -= luck
 # T: importance(1) or not important(0)
 # can only lose UP to k. want to lose high luck 
+# when lose, make sure to -= balance!
+
+# want to sort list by importance, descending order :o 
+# or just make a list of important contests and then sorting that :o
+# wonder if i can do it all in one loop.. but let's get the basic sol'n first
+# Complete the luckBalance function below.
+def luckBalance(k, contests):
+    max_luck_balance = 0
+    important = []
+    unimportant = []
+
+    for contest in contests:
+        luck, importance = contest
+        if importance == 1:
+            important.append(contest)
+        else:
+            unimportant.append(contest)
+    
+    important.sort(reverse = True)
+    print(k)
+    print(important)
+    wins = len(important) - k
+    print('must win', wins)
+    i = 1
+    while i <= wins:
+        win = important.pop()
+        luck, importance = win
+        max_luck_balance -= luck
+        print('i', i)
+        print('win', win)
+        print('luck', luck)
+        # print('importance', importance)
+        i += 1
+    print(max_luck_balance)
+    print(important)
+    for imp in important:
+        luck, importance = imp
+        max_luck_balance += luck
+    
+    for un in unimportant:
+        luck, importance = un
+        max_luck_balance += luck
+    return max_luck_balance
+
+# Tests
+contest1 = [5 1
+2 1
+1 1
+8 1
+10 0
+5 0]
+k = 3
+##################################################
+
+# Notes:
+# input: num - important contests that can be lost
+# output: num - maximum amount of luck 
+# start @ luck balance = 0
+# Each contest (L, T);
+# L: lose - balance += luck; win - balance -= luck
+# T: importance(1) or not important(0)
+# can only lose UP to k. want to lose high luck 
 
 # want to sort list by importance, descending order :o 
 # or just make a list of important contests and then sorting that :o
 # Complete the luckBalance function below.
-def luckBalance(k, contests):
+def luckBalance1(k, contests):
     max_luck_balance = 0
     print('num of important loses', k)
     # contests.sort(key = lambda x: x[1], reverse = True)
@@ -56,11 +118,3 @@ def luckBalance(k, contests):
 
     return max_luck_balance
 
-# Tests
-contest1 = [5 1
-2 1
-1 1
-8 1
-10 0
-5 0]
-k = 3
