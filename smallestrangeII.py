c851c84c8a9be6a0623633a0b3e6e5be0b8f5eab
Aileen Tran <aileen.tran27@gmail.com>
2020-10-20 17:46:20 -0700

    WIP

--- smallestrangeII.py
+++ smallestrangeII.py
@@ -18,6 +18,8 @@ Return the smallest possible difference between the maximum value of B and the m
 # find min and max of b
 # find difference and return it :o 
 # what to do if num == mean? :o 
+# hmm.. want the num == mean to be closer to the maximum number..
+# add and subtract. whatever is closer, put it list b
 
 def smallestRangeII(A, K):
     mean = 0
@@ -25,22 +27,38 @@ def smallestRangeII(A, K):
     B = []
     
     for num in A:
-        print('num', num)
         sum_num += num
    
-    mean = sum_num / len(A)
-
+    mean = sum_num // len(A)
+    mean_nums = []
     for num in A:
         if num > mean:
             b_num = num - K
             B.append(b_num)
-        else:
+        elif num < mean:
             b_num = num + K
             B.append(b_num)
-            
-    min_num = min(B)
-    max_num = max(B)
+        elif num == mean:
+            mean_nums.append(num)
+    print(B)
+    print(mean_nums)
     
+    max_num = max(B)
+    for num in mean_nums:
+        num1 = num + K
+        num2 = num - K
+        diff1 = abs(num1 - max_num)
+        diff2 = abs(num2 - max_num)
+
+        if diff1 < diff2:
+            B.append(num1)
+        else:
+            B.append(num2)
+    max_num = max(B)
+    min_num = min(B)
+
+    print('max_num', max_num)
+    print('min_num', min_num)
     return max_num - min_num
 
 # Tests
@@ -58,4 +76,12 @@ K3 = 3
 # Output: 3
 # Explanation: B = [4,6,3]
 
-print(smallestRangeII(A1, K1))
+A4 = [7, 8, 8]
+K4 = 5
+# expected 1
+
+A5 = [1,3,6]
+K5 = 3
+# expected: 3
+
+print(smallestRangeII(A4, K4))
