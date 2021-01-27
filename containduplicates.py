32fe54d89f2e755de68f1ff3fa9b62119e47b1fd
Aileen <aileen.tran27@gmail.com>
2021-01-26 16:23:38 -0800

    Initial solution

--- containduplicates.py
+++ containduplicates.py
@@ -11,8 +11,18 @@ once there's a match, return true
 outside loop, return false
 """
 def containsDuplicate(nums):
-    return
+    nums.sort()
+    for i in range(len(nums) - 1):
+        num1 = nums[i]
+        num2 = nums[i + 1]
+        if num1 == num2:
+            return True
+    return False
 
 nums1 = [1,2,3,1] #true
 nums2 = [1,2,3,4] #false
-nums3 = [1,1,1,3,3,4,3,2,4,2]
+nums3 = [1,1,1,3,3,4,3,2,4,2] #true
+
+print(containsDuplicate(nums1))
+print(containsDuplicate(nums2))
+print(containsDuplicate(nums3))
