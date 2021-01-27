bb622a92e8de2a214b2d1f27e4876303f88e0f40
Aileen <aileen.tran27@gmail.com>
2021-01-26 16:20:34 -0800

    Initial commit containduplicates

---
+++ containduplicates.py
@@ -0,0 +1,18 @@
+"""
+Leetcode - Contains Duplicate
+
+input: list of numbers
+output: boolean - true if has duplicates, false otherwise
+
+sorting the nums
+loop through nums looking at pairs (account for index range)
+once there's a match, return true
+
+outside loop, return false
+"""
+def containsDuplicate(nums):
+    return
+
+nums1 = [1,2,3,1] #true
+nums2 = [1,2,3,4] #false
+nums3 = [1,1,1,3,3,4,3,2,4,2]
