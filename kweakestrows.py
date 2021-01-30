4cc56cafc666991afee949daea3093ddb9ffdd38
Aileen <aileen.tran27@gmail.com>
2021-01-30 13:45:05 -0800

    Initial solution for kweakestrows.py

--- kweakestrows.py
+++ kweakestrows.py
@@ -11,25 +11,46 @@ or an array w/ idx = row and value = keys
 """
 
 def kWeakestRows(mat, k):
-    return
+    soldiers = {}
+    for r, row in enumerate(mat):
+        # print(row)
+        num_soldiers = 0
+        for col in row:
+            # print(col)
+            if col > 0:
+                num_soldiers += col
+            else:
+                break
+        soldiers[r] = num_soldiers
+
+    sorted_soldiers = sorted(soldiers.items(), key = lambda soldier: soldier[1])
+
+    weakest = []
+    for soldier in sorted_soldiers:
+        row, num = soldier
+        weakest.append(row)
+
+    return weakest[:k]
 
 
-mat1 =
-[[1,1,0,0,0],
- [1,1,1,1,0],
- [1,0,0,0,0],
- [1,1,0,0,0],
- [1,1,1,1,1]]
+mat1 = [
+[1,1,0,0,0],
+[1,1,1,1,0],
+[1,0,0,0,0],
+[1,1,0,0,0],
+[1,1,1,1,1]
+ ]
 k1 = 3
 #[2,0,3]
 
-mat2 =
-[[1,0,0,0],
- [1,1,1,1],
- [1,0,0,0],
- [1,0,0,0]]
+mat2 = [
+[1,0,0,0],
+[1,1,1,1],
+[1,0,0,0],
+[1,0,0,0]
+ ]
 k2 = 2
 #[0,2]
 
 print(kWeakestRows(mat1, k1))
-print(kWeakestRows(mat2, k2))
+# print(kWeakestRows(mat2, k2))
