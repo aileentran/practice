76390dddceb11b7235e13898f0fcd3d9adf58da7
Aileen <aileen.tran27@gmail.com>
2021-01-30 18:59:07 -0800

    Initial commit fibonacci.py

---
+++ fibonacci.py
@@ -0,0 +1,13 @@
+"""
+Practicing recursion w/Fibonacci
+
+input: num - how many of the sequence to output
+output: list of Fibonacci nums
+"""
+
+def fib(num):
+    
+    return
+
+print(fib(2))
+print(fib(4))
