07118cfd6f8845f00007fd74aa5742f3a178ff6c
Aileen <aileen.tran27@gmail.com>
2021-01-30 19:05:26 -0800

    Completed

--- fibonacci.py
+++ fibonacci.py
@@ -3,11 +3,19 @@ Practicing recursion w/Fibonacci
 
 input: num - how many of the sequence to output
 output: list of Fibonacci nums
+
+notes:
+starts with 0 and 1
+curr num is the sum of 2 previous nums
 """
 
-def fib(num):
-    
-    return
+def fibonacci(num):
+    if num == 1:
+        return 0
+    if num == 2:
+        return 1
 
-print(fib(2))
-print(fib(4))
+    return fibonacci(num - 1) + fibonacci(num - 2)
+
+# print(fib(1))
+print(fibonacci(9))
