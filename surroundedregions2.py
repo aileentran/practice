b227c09c92d33e9e063e208df3bc4c668e713bd2
Aileen <aileen.tran27@gmail.com>
2021-02-08 12:35:04 -0800

    Completed

--- surroundedregions2.py
+++ surroundedregions2.py
@@ -11,9 +11,48 @@ loop through grid and convert "!" to Os
 return the grid
 """
 def solve(board):
-    return
+    # looping through and finding Os along boundaries
+    for r, row in enumerate(board):
+        for c, col in enumerate(row):
+            # O's along boundaries + adjacent
+            if col == 'O' and (r == 0 or r == len(board) - 1 or c == 0 or c == len(board[0]) - 1):
+                boundary_dfs(board, r, c)
 
-    
+    # finding surrounded Os
+    # MUST be in separate loop or else surrounded Os connected to boundary preemptive flip to X
+    for r, row in enumerate(board):
+        for c, col in enumerate(row):
+            if col == 'O':
+                flip_dfs(board, r, c)
+
+    # convert boundary '!'s to Os
+    for r, row in enumerate(board):
+        for c, col in enumerate(row):
+            if col == '!':
+                board[r][c] = 'O'
+
+    return board
+
+def boundary_dfs(board, r, c):
+    if r < 0 or c < 0 or r >= len(board) or c >= len(board[0]) or board[r][c] != 'O':
+        return
+    board[r][c] = '!'
+    boundary_dfs(board, r - 1, c) #up
+    boundary_dfs(board, r + 1, c) #down
+    boundary_dfs(board, r , c - 1) #left
+    boundary_dfs(board, r, c + 1) #right
+
+
+def flip_dfs(board, r, c):
+    if r < 0 or c < 0 or r >= len(board) or c >= len(board[0]) or board[r][c] != 'O':
+        return
+    board[r][c] = 'X'
+    flip_dfs(board, r - 1, c) #up
+    flip_dfs(board, r + 1, c) #down
+    flip_dfs(board, r , c - 1) #left
+    flip_dfs(board, r, c + 1) #right
+
+
 board1 = [
 ['X', 'X', 'X', 'X'],
 ['X', 'O', 'O', 'X'],
@@ -43,5 +82,5 @@ X O O X
 X O O X
 X O X X
 """
-print(solve(board1))
-print(solve(board2))
+print('board1', solve(board1))
+print('board2', solve(board2))
