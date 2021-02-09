d47b6adbfcaf4a14db04fa8edd1fcfe5ca4fbf1c
Aileen <aileen.tran27@gmail.com>
2021-02-09 15:54:35 -0800

    Cleaned up spacing

--- implementtrie.py
+++ implementtrie.py
@@ -10,24 +10,20 @@ class Trie(object):
 
     def insert(self, word):
         curr = self.head
-
         for char in word:
             if char not in curr:
                 curr[char] = {}
             curr = curr[char]
-
         curr['*'] = True
         return curr
 
     def search(self, word):
         """Returns True if the word is in trie."""
         curr = self.head
-
         for char in word:
             if char not in curr:
                 return False
             curr = curr[char]
-
         if "*" not in curr:
             return False
         else:
@@ -36,21 +32,16 @@ class Trie(object):
     def startsWith(self, prefix):
         """Returns true if a word starts with this prefix, false otherwise."""
         curr = self.head
-
         for char in prefix:
             if char not in curr:
                 return False
             curr = curr[char]
-
         return True
 
-
-
 trie = Trie()
-
 print(trie.insert("apple"))
 print(trie.search("apple"))   #// returns true
 print(trie.search("app") )    #// returns false
-print(trie.startsWith("app"))#// returns true
+print(trie.startsWith("app")) #// returns true
 print(trie.insert("app"))
 print(trie.search("app"))     #// returns true
