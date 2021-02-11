5c56ad43f72955f1101ff98ee72ae7d9dad23dad
Aileen <aileen.tran27@gmail.com>
2021-02-10 22:41:48 -0800

    Initial solution

--- designaddsearchwords.py
+++ designaddsearchwords.py
@@ -9,10 +9,6 @@ methods to: add words, search words
 search words - '.' can replace any character
 """
 
-# class Trie():
-#     def __init__(self):
-#         self.head = {}
-
 class WordDictionary(object):
 
     def __init__(self):
@@ -34,6 +30,7 @@ class WordDictionary(object):
             curr = curr[char]
 
         curr['*'] = True
+        # return self.head
 
     def search(self, word):
         """
@@ -41,28 +38,43 @@ class WordDictionary(object):
         :rtype: bool
         """
         curr = self.head
-
+        # print('self.head', self.head)
+        # print('initial curr', curr)
         # compare character to current
-        for char in word:
+        for c, char in enumerate(word):
+            # print('char', char)
+            # print('curr inside loop', curr)
             # character is a letter but does not match current node
             if char != '.' and char not in curr:
                 return False
             # character is '.' and need to do dfs through Trie
             elif char == '.':
-                # wild_card = curr
-                # curr = curr[wild_card]
-            # character is a letter AND matches current node
+                return self.dfs(word, c, curr)
             else:
                 curr = curr[char]
 
-    def dfs(self, word, curr):
-        return
+        if '*' in curr:
+            return True
+        else:
+            return False
+
+    def dfs(self, word, c, curr):
+        if c >= len(word):
+            if '*' in curr:
+                return True
+            else:
+                return False
+
+        for key, val in curr.items():
+            if self.dfs(word, c + 1, val):
+                return True
+            return False
 
 wordDictionary = WordDictionary()
-wordDictionary.addWord("bad")
-wordDictionary.addWord("dad")
-wordDictionary.addWord("mad")
-# wordDictionary.search("pad") # return False
-# wordDictionary.search("bad") # return True
-# wordDictionary.search(".ad") # return True
-# wordDictionary.search("b..") # return True
+print(wordDictionary.addWord("bad"))
+print(wordDictionary.addWord("dad"))
+print(wordDictionary.addWord("mad"))
+print(wordDictionary.search("pad")) # return False
+print(wordDictionary.search("bad")) # return True
+print(wordDictionary.search(".ad")) # return True
+print(wordDictionary.search("b..")) # return True
