67d9faffa3175b6222cc960cdd65d7c33c80a66b
Aileen <aileen.tran27@gmail.com>
2021-02-14 12:41:04 -0800

    Initial commit

---
+++ numgoodpairs.py
@@ -0,0 +1,20 @@
+"""
+Leetcode - 1512. Number of Good Pairs
+
+input: list of nums
+output: num - number of "good" Pairs
+
+Notes:
+good pair - num[i] == num[j] and i < j
+"""
+
+def numIdenticalPairs(nums):
+    return
+
+nums1 = [1,2,3,1,1,3]
+nums2 = [1,1,1,1]
+nums3 = [1,2,3]
+
+print(numIdenticalPairs(nums1))
+print(numIdenticalPairs(nums2))
+print(numIdenticalPairs(nums3))
