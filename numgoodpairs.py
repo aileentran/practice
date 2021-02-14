eda3a8dc6e5d4b557892640766d662dd3fcfcf0e
Aileen <aileen.tran27@gmail.com>
2021-02-14 13:19:43 -0800

    Solved numgoodpairs.py

--- numgoodpairs.py
+++ numgoodpairs.py
@@ -6,14 +6,28 @@ output: num - number of "good" Pairs
 
 Notes:
 good pair - num[i] == num[j] and i < j
+
+Thoughts
+set a counter to 0
+nested loop through nums w/ inner
+if num1 == num2, increment counter.
 """
 
 def numIdenticalPairs(nums):
-    return
+    counter = 0
 
-nums1 = [1,2,3,1,1,3]
-nums2 = [1,1,1,1]
-nums3 = [1,2,3]
+    for i in range(len(nums) - 1):
+        num1 = nums[i]
+        for j in range(i + 1, len(nums)):
+            num2 = nums[j]
+            if num1 == num2:
+                counter += 1
+
+    return counter
+
+nums1 = [1,2,3,1,1,3] #4
+nums2 = [1,1,1,1] #6
+nums3 = [1,2,3] #0
 
 print(numIdenticalPairs(nums1))
 print(numIdenticalPairs(nums2))
