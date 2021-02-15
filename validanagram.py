31de0e94158ebc9db9c89fa9a58471e7bfff38f3
Aileen <aileen.tran27@gmail.com>
2021-02-15 13:09:17 -0800

    Initial commit

---
+++ validanagram.py
@@ -0,0 +1,24 @@
+"""
+Leetcode - 242. Valid Anagram
+
+input: two strings
+output: boolean - true if valid anagrams of each other
+
+thoughts
+sort both strings in alphabetical order
+see if they are equal - true if equal, false if not
+"""
+
+def isAnagram(s, t):
+    return
+
+s1 = "anagram"
+t1 = "nagaram"
+#True
+
+s2 = "rat"
+t2 = "car"
+#False
+
+print(isAnagram(s1, t1))
+print(isAnagram(s2, t2))
