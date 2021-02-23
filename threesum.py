ff9583cd3caa2ee30a55e7e84afa46f0177448ec
Aileen <aileen.tran27@gmail.com>
2021-02-23 11:43:12 -0800

    Initial commit with main function
    - Created `threeSum` function
    - Added pseudocode outlining steps
    - Created test cases and print statements

---
+++ threesum.py
@@ -0,0 +1,30 @@
+"""
+Leetcode - 15. 3Sum
+
+input: list of nums
+output: list of unique triplets that sum to 0
+
+Notes:
+no duplicate triplets
+can reuse some numbers
+
+Thoughts
+empty results list
+
+use a triple nested loop to go through the list
+
+return results
+
+think of another solution that's less than O^3 time
+"""
+
+def threeSum(nums):
+    return
+
+nums1 = [-1,0,1,2,-1,-4] #[[-1,-1,2],[-1,0,1]]
+nums2 = [] #[]
+nums3 = [0] #[]
+
+print(threeSum(nums1))
+print(threeSum(nums2))
+print(threeSum(nums3))
