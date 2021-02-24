d5d17b446cc7fe30329e56bf7891b936ddfdb018
Aileen <aileen.tran27@gmail.com>
2021-02-24 11:27:07 -0800

    Initial commit of `twosumII.py`
    - Included details of the problem
    - Created `twoSum` function
    - Set up three test cases

---
+++ twosumII.py
@@ -0,0 +1,21 @@
+"""
+Leetcode - 167. Two Sum II - Input array is sorted
+
+input: sorted list of nums, target num
+output: list of two nums - idxs that sum to target
+
+Note
+have exactly 1 solution and cannot reuse numbers
+1 <= index <= length of list
+"""
+def twoSum(numbers, target):
+    return
+
+numbers = [2,7,11,15], target = 9
+# [1, 2] bc 2 + 7 = 9
+
+numbers = [2,3,4], target = 6
+# [1, 3] bc 2 + 4 = 6
+
+numbers = [-1,0], target = -1
+# [1, 2] bc -1 + 0 = -1
