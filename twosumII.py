3006d66ae5f0234023bca6baeaf1e73bd4f6b6ff
Aileen <aileen.tran27@gmail.com>
2021-02-24 11:35:22 -0800

    Completed `twosumII.py` file
    - Finished writing `twoSum` function
    - Included print statements for testing

--- twosumII.py
+++ twosumII.py
@@ -9,13 +9,31 @@ have exactly 1 solution and cannot reuse numbers
 1 <= index <= length of list
 """
 def twoSum(numbers, target):
-    return
 
-numbers = [2,7,11,15], target = 9
+    left = 0
+    right = len(numbers) - 1
+
+    while left < right:
+        sum = numbers[left] + numbers[right]
+        if sum < target:
+            left += 1
+        elif sum > target:
+            right -= 1
+        else:
+            return [left + 1, right + 1]
+
+numbers1 = [2,7,11,15]
+target1 = 9
 # [1, 2] bc 2 + 7 = 9
 
-numbers = [2,3,4], target = 6
+numbers2 = [2,3,4]
+target2 = 6
 # [1, 3] bc 2 + 4 = 6
 
-numbers = [-1,0], target = -1
+numbers3 = [-1,0]
+target3 = -1
 # [1, 2] bc -1 + 0 = -1
+
+print(twoSum(numbers1, target1))
+print(twoSum(numbers2, target2))
+print(twoSum(numbers3, target3))
