4bf5bb40735957c2aaaa70c3b60ed7c55e88b6d8
Aileen <aileen.tran27@gmail.com>
2021-02-25 10:28:23 -0800

    Completed `maxProfit` function but WIP optimal solution
    - Solved `maxProfit` using brute force
    - Timed out at large input
    - WIP optimal solution using two pointers

--- buyandsellstock2.py
+++ buyandsellstock2.py
@@ -18,7 +18,16 @@ then set the profit variable to the new difference
 outside of the loop, return profit
 """
 def maxProfit(prices):
-    return
+    profit = 0
+
+    for p in range(len(prices) - 1):
+        buy = prices[p]
+        max_sell = max(prices[p + 1:])
+
+        if max_sell > buy and (max_sell - buy) > profit:
+            profit = max_sell - buy
+
+    return profit
 
 prices1 = [7,1,5,3,6,4] #5 = 6 (day 5) - 1 (day 2)
 prices2 = [7,6,4,3,1] #0 - no transactions
