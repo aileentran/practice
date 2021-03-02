ba3598e1d186b103f4ca62a5282db6054f4f440a
Aileen <aileen.tran27@gmail.com>
2021-03-02 12:40:33 -0800

    Completed `twosumii2.py` using two pointers
    - Added pseudocode on how to approach the problem
    - Completed `twoSum` function
    - Passed all test cases

--- twosumii2.py
+++ twosumii2.py
@@ -2,15 +2,34 @@
 Leetcode - 167. Two Sum II - Input array is sorted
 
 input: list of nums, target num
-output: two indices that sum to target in a list
+output: two indices (or.. position rather = i + 1) that sum to target in a list
 
 Notes:
 exactly one solution
 list of nums in ascending order
 cannot use same element twice
+
+
+Thoughts:
+Have two pointers, one at the beginning and one at the end
+hmm.. do i need to skip nums that are == to num before?
+if the sum of two numbers is < target, move right pointer
+if sum of two numbers > target, move left pointer
+once two numbers == target, return list of indices
 """
 def twoSum(numbers, target):
-    return
+    left = 0
+    right = len(numbers) - 1
+
+    while left <= len(numbers) and left < right:
+        sum = numbers[left] + numbers[right]
+        if sum < target:
+            left += 1
+        elif sum > target:
+            right -= 1
+        else:
+            return [left + 1, right + 1]
+
 
 numbers1 = [2,7,11,15]
 target1 = 9
@@ -22,6 +41,7 @@ target2 = 6
 
 numbers3 = [-1,0]
 target3 = -1
+# [1, 2]
 
 print(twoSum(numbers1, target1))
 print(twoSum(numbers2, target2))
