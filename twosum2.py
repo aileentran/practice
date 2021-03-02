376036aeaa0c40ada6417cbd16f9e76d2520c8af
Aileen <aileen.tran27@gmail.com>
2021-03-02 09:53:43 -0800

    Initial commit of `twosum2.py`
    - Added details of problem
    - Created `twoSum` function
    - Added 3 test cases

---
+++ twosum2.py
@@ -0,0 +1,26 @@
+"""
+Leetcode - 1. Two Sum
+
+input: list of nums, num - target
+output: list of 2 nums - nums that sum to target
+
+Notes:
+has exactly 1 solution
+cannot use the same num twice
+return in any order
+"""
+
+def twoSum(nums, target):
+    return
+
+nums1 = [2,7,11,15]
+target1 = 9
+#Because nums[0] + nums[1] == 9, we return [0, 1].
+
+nums2 = [3,2,4]
+target2 = 6
+#[1,2]
+
+nums3 = [3,3]
+target3 = 6
+# [0,1]
