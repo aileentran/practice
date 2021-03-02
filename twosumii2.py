ed335dd09cc51adf0c4f4629c5602bf3377eb9ec
Aileen <aileen.tran27@gmail.com>
2021-03-02 10:47:00 -0800

    Initial commit of `twosumii2.py`
    - Added details of the problem
    - Created `twoSum` function
    - Added 3 test cases and print statements

---
+++ twosumii2.py
@@ -0,0 +1,28 @@
+"""
+Leetcode - 167. Two Sum II - Input array is sorted
+
+input: list of nums, target num
+output: two indices that sum to target in a list
+
+Notes:
+exactly one solution
+list of nums in ascending order
+cannot use same element twice
+"""
+def twoSum(numbers, target):
+    return
+
+numbers1 = [2,7,11,15]
+target1 = 9
+# [1,2]
+
+numbers2 = [2,3,4]
+target2 = 6
+# [1,3]
+
+numbers3 = [-1,0]
+target3 = -1
+
+print(twoSum(numbers1, target1))
+print(twoSum(numbers2, target2))
+print(twoSum(numbers3, target3))
