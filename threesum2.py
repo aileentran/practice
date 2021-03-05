7ad099d605db34c99625d3340efcec7aa992d0c1
Aileen <aileen.tran27@gmail.com>
2021-03-05 11:48:53 -0800

    Initial commit for `threesum2.py`
    - Added details of the problem
    - Set up main function `threeSum`
    - Included 3 test cases and print statements

---
+++ threesum2.py
@@ -0,0 +1,13 @@
+"""
+Leetcode - 15. 3Sum
+
+input: list of nums
+output: list of unique triplets that each sum to 0
+"""
+
+def threeSum(nums):
+    return
+
+nums = [-1,0,1,2,-1,-4] #[[-1,-1,2],[-1,0,1]]
+nums = [] #[]
+nums = [0] #[]
