d93b2204365a17d36099ff77fb2c9be52c03d500
Aileen <aileen.tran27@gmail.com>
2021-03-09 10:53:15 -0800

    Initial solution for `productexceptself3.py`
    - Completed `productExceptSelf` function using a nested loop
    - Timed out for large input
    - WIP for O(n) time solution

--- productexceptself3.py
+++ productexceptself3.py
@@ -6,7 +6,12 @@ output: list of nums - product of all other eles except @ that index
 """
 
 def productExceptSelf(nums):
-    return
+    result = [1] * len(nums)
+    for n, num in enumerate(nums):
+        for r in range(len(result)):
+            if n != r:
+                result[r] *= num
+    return result
 
 nums = [1,2,3,4]
 # [24,12,8,6]
