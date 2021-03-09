cb00c19bb89c5f9cc58b4cbd1288fab163a4195c
Aileen <aileen.tran27@gmail.com>
2021-03-09 10:34:11 -0800

    Initial commit of `productexceptself3.py`
    - Added details about the problem
    - Set up `productExceptSelf` function
    - Included one test case and print statement

---
+++ productexceptself3.py
@@ -0,0 +1,14 @@
+"""
+Leetcode - 238. Product of Array Except Self
+
+input: list of nums
+output: list of nums - product of all other eles except @ that index
+"""
+
+def productExceptSelf(nums):
+    return
+
+nums = [1,2,3,4]
+# [24,12,8,6]
+
+print(productExceptSelf(nums))
