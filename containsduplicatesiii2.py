37b4ef03ed41e408b19558672902bc7674e0ca36
Aileen <aileen.tran27@gmail.com>
2021-03-17 16:47:51 -0700

    Updated pseudocode for `containsduplicatesiii2.py` to reflect current solution

--- containsduplicatesiii2.py
+++ containsduplicatesiii2.py
@@ -4,10 +4,34 @@ Leetcode - 220. Contains Duplicate III
 input: list of nums, k(idx), t(value)
 output: boolean
 True if diff of vals <= t and diff of idx <= k
+
+Thoughts
+create an empty dictionary
+key - num, val - the latest index
+
+loop through nums using range
+nested loop to check value in dictionary
+if the value is within num +- t and both idxs are within k range, return true 
+
+outside loop return false
 """
 
 def containsNearbyAlmostDuplicate(nums, k, t):
-    return
+    print(nums)
+    print('k index', k)
+    print('t value', t)
+    dict = {}
+    for i, num in enumerate(nums):
+        print('i', i)
+        print('num', num)
+        print(dict)
+        for val in dict:
+            if num - t <= val <= num + t and abs(dict[val] - i) <= k:
+                return True
+        dict[num] = i
+        # print(dict)
+        print('\n')
+    return False
 
 nums1 = [1,2,3,1]
 k1 = 3
@@ -22,6 +46,12 @@ k3 = 2
 t3 = 3
 # False
 
-print(containsNearbyAlmostDuplicate(nums1, k1, t1))
-print(containsNearbyAlmostDuplicate(nums2, k2, t2))
+nums4 = [8,7,15,1,6,1,9,15]
+k4 = 1
+t4 = 3
+# True
+
+# print(containsNearbyAlmostDuplicate(nums1, k1, t1))
+# print(containsNearbyAlmostDuplicate(nums2, k2, t2))
 print(containsNearbyAlmostDuplicate(nums3, k3, t3))
+# print(containsNearbyAlmostDuplicate(nums4, k4, t4))
