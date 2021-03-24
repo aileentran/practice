d9056819ca9156f6fc48afb15fdab3c2dc265143
Aileen <aileen.tran27@gmail.com>
2021-03-23 17:41:28 -0700

    Initial commit `climbingstairs.py`
    - Added details of the problem
    - Created `climbStairs` function
    - Included 2 tests and print statements

---
+++ climbingstairs.py
@@ -0,0 +1,25 @@
+"""
+Leetcode - 70. Climbing Stairs
+
+input: num - number of steps
+output: num - number of distinct ways to get to the top == input
+"""
+
+def climbStairs(n):
+    return
+
+n1 = 2
+# Output: 2
+# Explanation: There are two ways to climb to the top.
+# 1. 1 step + 1 step
+# 2. 2 steps
+
+n2 = 3
+# Output: 3
+# Explanation: There are three ways to climb to the top.
+# 1. 1 step + 1 step + 1 step
+# 2. 1 step + 2 steps
+# 3. 2 steps + 1 step
+
+print(climbStairs(n1))
+print(climbStairs(n2))
