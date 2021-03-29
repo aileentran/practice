5cc0e9bea503cbdac46e3ab88fae9d68bd128f42
Aileen <aileen.tran27@gmail.com>
2021-03-29 10:42:59 -0700

    Initial commit of `numinlist.py`
    - Added details of the problem
    - Created `num_in_list` function
    - Included 3 test cases and print statements

---
+++ numinlist.py
@@ -0,0 +1,20 @@
+"""
+input: list of nums, num
+output: boolean - true if num in list, false otherwise
+"""
+
+def num_in_list(list, num):
+    return
+
+list1 = [9, 5, 3, 1, 4]
+num1 = 3
+# True
+list2 = []
+num2 = 3
+# False
+list3 = [1, 2, 3, 4]
+num3 = 2
+
+print(num_in_list(list1, num1))
+print(num_in_list(list2, num2))
+print(num_in_list(list3, num3))
