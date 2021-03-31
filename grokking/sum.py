1a191430ccba48727d4b23a34981eb5b2df9d2fc
Aileen <aileen.tran27@gmail.com>
2021-03-31 14:32:50 -0700

    Created separate folder for Grokking practice
    - Created `sum.py` for problem 4.1
    - Added details of problem and pseudocode
    - Created `sum` function
    - included 2 test cases and respective print statements

---
+++ grokking/sum.py
@@ -0,0 +1,21 @@
+"""
+Grokking ch. 4
+
+Write recursive solution for summing nums
+input: list of nums
+output: num - sum of all nums in list
+
+Thoughts
+base case = empty list, return 0
+
+return num (first int of list) + func(rest of list)
+"""
+
+def sum(nums):
+    return
+
+nums1 = [2, 4, 6] #12
+nums2 = [] #0
+
+print(sum(nums1))
+print(sum(nums2))
