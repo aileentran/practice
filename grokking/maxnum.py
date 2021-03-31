0b03f2f4d25e3cee12abc846e04b0cc9ae122763
Aileen <aileen.tran27@gmail.com>
2021-03-31 15:07:04 -0700

    Initial commit of `maxnum.py`
    - Added details of the problem
    - Included pseudocode on approach
    - Added 2 test cases and respective print statements

---
+++ grokking/maxnum.py
@@ -0,0 +1,21 @@
+"""
+Grokking Ch. 4
+
+4.3 Find the maximum number in list
+input: list of nums
+output: num, the largest num in the list
+
+Thoughts
+base case: list is empty, return 0
+
+return max(first int, func(rest of list))
+"""
+
+def max(nums):
+    return
+
+nums1 = [9, 3, 8, 4, -1]
+nums2 = []
+
+print(max(nums1))
+print(max(nums2))
