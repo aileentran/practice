5e32a9880c4fbdeb6d37a0c1cc5a0ac4ea6aa8fa
Aileen <aileen.tran27@gmail.com>
2021-03-31 15:13:33 -0700

    Completed `maxnum.py` using recursion
    - Added 1 more test case
    - Finished `maximum` function, renamed from `max`
    - Passed all test cases

--- grokking/maxnum.py
+++ grokking/maxnum.py
@@ -11,11 +11,17 @@ base case: list is empty, return 0
 return max(first int, func(rest of list))
 """
 
-def max(nums):
-    return
+def maximum(nums):
+    if nums == []:
+        return None
+    if len(nums) == 1:
+        return nums[0]
+    return max(nums[0], maximum(nums[1:]))
 
-nums1 = [9, 3, 8, 4, -1]
-nums2 = []
+nums1 = [9, 3, 8, 4, -1] #9
+nums2 = [] #None
+nums3 =[7] #7
 
-print(max(nums1))
-print(max(nums2))
+print(maximum(nums1))
+print(maximum(nums2))
+print(maximum(nums3))
