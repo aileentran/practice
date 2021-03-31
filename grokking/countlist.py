32632112b3486ae736f1fa7d999da64efdba3a84
Aileen <aileen.tran27@gmail.com>
2021-03-31 14:45:33 -0700

    Created `countlist.py` for Grokking 4.2
    - Added conditions of the problem
    - Included pseudocode on approach
    - Created `count` function
    - Included 2 test cases and print statements

---
+++ grokking/countlist.py
@@ -0,0 +1,21 @@
+"""
+Grokking Ch. 4
+
+4.2 WRite a recursive function to count the number of items in a list
+input: list of.. things
+output: num - number of items in the list
+
+Thoughts
+base case: empty list, return 0
+
+return 1 + func(list[1:])
+"""
+
+def count(list):
+    return
+
+list1 = ['a', 1, 'C'] #3
+list2 = [] #0
+
+print(count(list1))
+print(count(list2))
