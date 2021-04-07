a9d550364f8359c3dabc3dea086e1ccfe8bfce08
Aileen <aileen.tran27@gmail.com>
2021-04-07 15:39:05 -0700

    Initial commit of `mergsortedll.py`
    - Added details of the problem
    - Set up class for singly-linked lists
    - Created `mergeTwoLists` function

---
+++ mergesortedll.py
@@ -0,0 +1,12 @@
+"""
+Leetcode - 21. Merge Two Sorted Lists
+
+input: head of 2 linked Lists
+output: head of merged linked list
+"""
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+def mergeTwoLists(l1, l2):
+    return
